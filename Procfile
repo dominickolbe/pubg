1371c7c129256fbe1410af06de781d76d714ae4f
Dominic Kolbe <kolbedominic@gmail.com>
2020-05-29 02:38:31 +0100

    Add server

--- Procfile
+++ Procfile
@@ -1,2 +1,4 @@
+web: npm start
+
 importplayer: yarn workspace pubg-server ts-node --transpile-only ./src/scripts/importplayer.ts
 importlifetimestats: yarn workspace pubg-server ts-node --transpile-only ./src/scripts/importlifetimestats.ts
--- packages/pubg-server/src/index.ts
+++ packages/pubg-server/src/index.ts
@@ -1,7 +1,35 @@
 require("dotenv-safe").config();
+import Koa from "koa";
+import Router from "koa-router";
+import {
+  HTTP_STATUS_OK,
+  HTTP_STATUS_INTERNAL_SERVER_ERROR,
+} from "pubg-utils/src";
+
+const PORT = process.env.PORT;
 
 const server = async () => {
-  // WIP
+  const app = new Koa();
+
+  app.use(async (ctx, next) => {
+    try {
+      await next();
+    } catch (err) {
+      ctx.status = err.status || HTTP_STATUS_INTERNAL_SERVER_ERROR;
+      ctx.app.emit("error", err, ctx);
+    }
+  });
+
+  const router = new Router();
+
+  router.get("/", async (ctx) => {
+    ctx.body = "ok.";
+    ctx.response.status = HTTP_STATUS_OK;
+  });
+
+  app.use(router.routes());
+
+  app.listen(PORT, () => console.log(`server running on port ${PORT}`));
 };
 
 server();
