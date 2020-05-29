3180d1d6d5a0b61be46ac05784c57f75bf3c0c86
Dominic Kolbe <kolbedominic@gmail.com>
2020-05-30 00:22:39 +0100

    Add API endpoint GET player

--- packages/pubg-server/src/index.ts
+++ packages/pubg-server/src/index.ts
@@ -2,13 +2,19 @@ require("dotenv-safe").config();
 import Koa from "koa";
 import Router from "koa-router";
 import {
-  HTTP_STATUS_OK,
   HTTP_STATUS_INTERNAL_SERVER_ERROR,
+  HTTP_STATUS_NOT_FOUND,
+  HTTP_STATUS_OK,
 } from "pubg-utils/src";
+import { Database } from "./database";
+import { PlayerDbController } from "./database/model/player";
 
 const PORT = process.env.PORT;
 
 const server = async () => {
+  const result = await Database.connect();
+  if (result.err) process.exit(1);
+
   const app = new Koa();
 
   app.use(async (ctx, next) => {
@@ -22,19 +28,25 @@ const server = async () => {
 
   const router = new Router();
 
-  router.get("/", async (ctx) => {
+  router.get("/api/v1/status", async (ctx) => {
     ctx.body = "ok.";
     ctx.response.status = HTTP_STATUS_OK;
   });
 
-  router.get("/api/tatus", async (ctx) => {
-    ctx.body = "ok.";
-    ctx.response.status = HTTP_STATUS_OK;
+  router.get("/api/v1/players/:id", async (ctx) => {
+    const player = await PlayerDbController.findByName(ctx.params.id);
+    if (player.ok) {
+      ctx.body = player.val;
+    } else {
+      ctx.response.status = HTTP_STATUS_NOT_FOUND;
+    }
   });
 
   app.use(router.routes());
 
-  app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+  app.listen(PORT, () =>
+    console.log(`[Info]: server is running on port ${PORT}`)
+  );
 };
 
 server();
