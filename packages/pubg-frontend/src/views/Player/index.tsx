9951fa26367af43f4c8b22e93f85be291d108e37
Dominic Kolbe <kolbedominic@gmail.com>
2020-06-03 00:25:15 +0100

    Show more stats for player

--- packages/pubg-frontend/src/views/Player/index.tsx
+++ packages/pubg-frontend/src/views/Player/index.tsx
@@ -1,10 +1,14 @@
 import { Container, Grid, Typography } from "@material-ui/core";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
+import Collapse from "@material-ui/core/Collapse";
+import Divider from "@material-ui/core/Divider";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import { makeStyles } from "@material-ui/core/styles";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
 import { css } from "emotion";
 import { PlayerRequest } from "pubg-model/types/Player";
 import React, { ReactNode, useEffect, useState } from "react";
@@ -57,6 +61,8 @@ export const Player = () => {
 
   const [player, setPlayer] = useState<PlayerRequest | null>(null);
 
+  const [openStats, setOpenStats] = useState(false);
+
   useEffect(() => {
     loadPlayer();
   }, []);
@@ -103,6 +109,36 @@ export const Player = () => {
                 label="Top 10"
                 value={() => totalStats?.top10s || "-"}
               />
+              <Divider light />
+              <ListItem
+                button
+                onClick={() => setOpenStats(!openStats)}
+                className={css`
+                  display: flex;
+                  justify-content: center;
+                `}
+              >
+                {openStats ? <ExpandLess /> : <ExpandMore />}
+              </ListItem>
+              <Divider light />
+              <Collapse in={openStats} timeout="auto">
+                <SingleStatsListItem
+                  label="dBNOs"
+                  value={() => totalStats?.dBNOs || "-"}
+                />
+                <SingleStatsListItem
+                  label="assists"
+                  value={() => totalStats?.assists || "-"}
+                />
+                <SingleStatsListItem
+                  label="revives"
+                  value={() => totalStats?.revives || "-"}
+                />
+                <SingleStatsListItem
+                  label="vehicleDestroys"
+                  value={() => totalStats?.vehicleDestroys || "-"}
+                />
+              </Collapse>
             </List>
           </Card>
         </Grid>
