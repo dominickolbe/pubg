6d8b2702c63df2ba0e9d1dca454e00cb8002f22e
Dominic Kolbe <kolbedominic@gmail.com>
2020-06-12 17:37:26 +0100

    Update matches table

--- packages/pubg-frontend/src/components/PlayerMatchesList/index.tsx
+++ packages/pubg-frontend/src/components/PlayerMatchesList/index.tsx
@@ -1,3 +1,4 @@
+import Box from "@material-ui/core/Box";
 import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
@@ -5,12 +6,14 @@ import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
 import TableRow from "@material-ui/core/TableRow";
+import Typography from "@material-ui/core/Typography";
 import { formatDistanceToNow, parseISO } from "date-fns";
 import orderBy from "lodash/orderBy";
 import { MatchesRequest } from "pubg-model/types/Match";
 import { PlayerRequest } from "pubg-model/types/Player";
-import React from "react";
+import React, { useState } from "react";
 import {
   formatNumber,
   getGameMode,
@@ -40,50 +43,96 @@ export const PlayerMatchesList = (props: {
 
   const orderedMatches = orderBy(matches, ["createdAt"], ["desc"]);
 
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [page, setPage] = useState(0);
+
   return (
     <TableContainer component={Paper}>
       <Table>
         <TableHead>
           <TableRow>
-            <TableCell>Rank</TableCell>
-            <TableCell>Mode</TableCell>
-            <TableCell>Map</TableCell>
-            <TableCell align="right">Kills</TableCell>
-            <TableCell align="right">Damage</TableCell>
+            <TableCell>
+              <Typography component="div">
+                <Box fontWeight="fontWeightMedium" fontSize={13}>
+                  Rank
+                </Box>
+              </Typography>
+            </TableCell>
+            <TableCell>
+              <Typography component="div">
+                <Box fontWeight="fontWeightMedium" fontSize={13}>
+                  Mode
+                </Box>
+              </Typography>
+            </TableCell>
+            <TableCell>
+              <Typography component="div">
+                <Box fontWeight="fontWeightMedium" fontSize={13}>
+                  Map
+                </Box>
+              </Typography>
+            </TableCell>
+            <TableCell align="right">
+              <Typography component="div">
+                <Box fontWeight="fontWeightMedium" fontSize={13}>
+                  Kills
+                </Box>
+              </Typography>
+            </TableCell>
+            <TableCell align="right">
+              <Typography component="div">
+                <Box fontWeight="fontWeightMedium" fontSize={13}>
+                  Damage
+                </Box>
+              </Typography>
+            </TableCell>
             <TableCell align="right"></TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {orderedMatches.map((match) => {
-            const playerStats = getPlayerMatchStats(match, player.pubgId);
-            return (
-              <TableRow key={match.matchId} hover>
-                <TableCell component="th" scope="row">
-                  {playerStats.winPlace} / {match.teams.length}
-                </TableCell>
-                <TableCell component="th" scope="row">
-                  {getGameMode(match.gameMode)}
-                </TableCell>
-                <TableCell component="th" scope="row">
-                  {getMapName(match.mapName)}
-                </TableCell>
-                <TableCell component="th" scope="row" align="right">
-                  {playerStats.kills}
-                </TableCell>
-                <TableCell component="th" scope="row" align="right">
-                  {formatNumber(Math.ceil(playerStats.damageDealt))}
-                </TableCell>
-                <TableCell align="right">
-                  {" "}
-                  {formatDistanceToNow(parseISO(match.createdAt), {
-                    addSuffix: true,
-                  })}
-                </TableCell>
-              </TableRow>
-            );
-          })}
+          {orderedMatches
+            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            .map((match) => {
+              const playerStats = getPlayerMatchStats(match, player.pubgId);
+              return (
+                <TableRow key={match.matchId} hover>
+                  <TableCell component="th" scope="row">
+                    {playerStats.winPlace} / {match.teams.length}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {getGameMode(match.gameMode)}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {getMapName(match.mapName)}
+                  </TableCell>
+                  <TableCell component="th" scope="row" align="right">
+                    {playerStats.kills}
+                  </TableCell>
+                  <TableCell component="th" scope="row" align="right">
+                    {formatNumber(Math.ceil(playerStats.damageDealt))}
+                  </TableCell>
+                  <TableCell align="right">
+                    {" "}
+                    {formatDistanceToNow(parseISO(match.createdAt), {
+                      addSuffix: true,
+                    })}
+                  </TableCell>
+                </TableRow>
+              );
+            })}
         </TableBody>
       </Table>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={matches.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={(event, newPage) => setPage(newPage)}
+        onChangeRowsPerPage={(event) =>
+          setRowsPerPage(parseInt(event.target.value, 10))
+        }
+      />
     </TableContainer>
   );
 };
