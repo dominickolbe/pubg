ffd4ef772ab3a59abaebccf0582851b2e870f80b
Dominic Kolbe <kolbedominic@gmail.com>
2020-06-15 00:58:12 +0100

    Update match table

--- packages/pubg-frontend/src/components/PlayerMatchesList/index.tsx
+++ packages/pubg-frontend/src/components/PlayerMatchesList/index.tsx
@@ -47,83 +47,98 @@ export const PlayerMatchesList = (props: {
   const [page, setPage] = useState(0);
 
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>
-              <Typography component="div">
-                <Box fontWeight="fontWeightMedium" fontSize={13}>
-                  Rank
-                </Box>
-              </Typography>
-            </TableCell>
-            <TableCell>
-              <Typography component="div">
-                <Box fontWeight="fontWeightMedium" fontSize={13}>
-                  Mode
-                </Box>
-              </Typography>
-            </TableCell>
-            <TableCell>
-              <Typography component="div">
-                <Box fontWeight="fontWeightMedium" fontSize={13}>
-                  Map
-                </Box>
-              </Typography>
-            </TableCell>
-            <TableCell align="right">
-              <Typography component="div">
-                <Box fontWeight="fontWeightMedium" fontSize={13}>
-                  Kills
-                </Box>
-              </Typography>
-            </TableCell>
-            <TableCell align="right">
-              <Typography component="div">
-                <Box fontWeight="fontWeightMedium" fontSize={13}>
-                  Damage
-                </Box>
-              </Typography>
-            </TableCell>
-            <TableCell align="right"></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {orderedMatches
-            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-            .map((match) => {
-              const playerStats = getPlayerMatchStats(match, player.pubgId);
-              return (
-                <TableRow key={match.matchId} hover>
-                  <TableCell component="th" scope="row">
-                    {playerStats.winPlace} / {match.teams.length}
-                  </TableCell>
-                  <TableCell component="th" scope="row">
-                    {getGameMode(match.gameMode)}
-                  </TableCell>
-                  <TableCell component="th" scope="row">
-                    {getMapName(match.mapName)}
-                  </TableCell>
-                  <TableCell component="th" scope="row" align="right">
-                    {playerStats.kills}
-                  </TableCell>
-                  <TableCell component="th" scope="row" align="right">
-                    {formatNumber(Math.ceil(playerStats.damageDealt))}
-                  </TableCell>
-                  <TableCell align="right">
-                    {" "}
-                    {formatDistanceToNow(parseISO(match.createdAt), {
-                      addSuffix: true,
-                    })}
-                  </TableCell>
-                </TableRow>
-              );
-            })}
-        </TableBody>
-      </Table>
+    <Paper>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <Typography component="div">
+                  <Box fontWeight="fontWeightMedium" fontSize={13}>
+                    Rank
+                  </Box>
+                </Typography>
+              </TableCell>
+              <TableCell>
+                <Typography component="div">
+                  <Box fontWeight="fontWeightMedium" fontSize={13}>
+                    Mode
+                  </Box>
+                </Typography>
+              </TableCell>
+              <TableCell>
+                <Typography component="div">
+                  <Box fontWeight="fontWeightMedium" fontSize={13}>
+                    Map
+                  </Box>
+                </Typography>
+              </TableCell>
+              <TableCell align="right">
+                <Typography component="div">
+                  <Box fontWeight="fontWeightMedium" fontSize={13}>
+                    Kills
+                  </Box>
+                </Typography>
+              </TableCell>
+              <TableCell align="right">
+                <Typography component="div">
+                  <Box fontWeight="fontWeightMedium" fontSize={13}>
+                    Damage
+                  </Box>
+                </Typography>
+              </TableCell>
+              <TableCell align="right"></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {orderedMatches
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((match) => {
+                const playerStats = getPlayerMatchStats(match, player.pubgId);
+                return (
+                  <TableRow key={match.matchId} hover>
+                    <TableCell
+                      component="th"
+                      scope="row"
+                      style={{
+                        minWidth: "80px",
+                      }}
+                    >
+                      {playerStats.winPlace} / {match.teams.length}
+                    </TableCell>
+                    <TableCell component="th" scope="row">
+                      {getGameMode(match.gameMode)}
+                    </TableCell>
+                    <TableCell component="th" scope="row">
+                      {getMapName(match.mapName)}
+                    </TableCell>
+                    <TableCell component="th" scope="row" align="right">
+                      {playerStats.kills}
+                    </TableCell>
+                    <TableCell component="th" scope="row" align="right">
+                      {formatNumber(Math.ceil(playerStats.damageDealt))}
+                    </TableCell>
+                    <TableCell
+                      component="th"
+                      scope="row"
+                      align="right"
+                      style={{
+                        minWidth: "150px",
+                      }}
+                    >
+                      {formatDistanceToNow(parseISO(match.createdAt), {
+                        addSuffix: true,
+                      })}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
       <TablePagination
-        rowsPerPageOptions={[5, 10, 25]}
+        labelRowsPerPage=""
+        rowsPerPageOptions={[5, 10, 25, 50, 100]}
         component="div"
         count={matches.length}
         rowsPerPage={rowsPerPage}
@@ -133,6 +148,6 @@ export const PlayerMatchesList = (props: {
           setRowsPerPage(parseInt(event.target.value, 10))
         }
       />
-    </TableContainer>
+    </Paper>
   );
 };
