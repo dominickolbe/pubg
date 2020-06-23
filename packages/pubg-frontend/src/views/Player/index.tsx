23c272fc7c0763c0508927cb771f4545d305b45f
Dominic Kolbe <kolbedominic@gmail.com>
2020-06-23 14:34:07 +0100

    Add player to favorite

--- packages/pubg-frontend/src/views/Player/index.tsx
+++ packages/pubg-frontend/src/views/Player/index.tsx
@@ -2,11 +2,14 @@
 
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
 import List from "@material-ui/core/List";
 import Snackbar from "@material-ui/core/Snackbar";
 import { makeStyles } from "@material-ui/core/styles";
 import Tooltip from "@material-ui/core/Tooltip";
 import Typography from "@material-ui/core/Typography";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import Alert from "@material-ui/lab/Alert";
 import Skeleton from "@material-ui/lab/Skeleton";
 import { formatDistanceToNow, isBefore, parseISO, sub } from "date-fns";
@@ -14,6 +17,7 @@ import { MatchesRequest } from "pubg-model/types/Match";
 import { PlayerRequest } from "pubg-model/types/Player";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useLocalStorage } from "react-use";
 import { ApiController } from "../../components/ApiController";
 import { PlayerMatchesList } from "../../components/PlayerMatchesList";
 import {
@@ -33,8 +37,16 @@ const useStyles = makeStyles((theme) => ({
     paddingTop: theme.spacing(3),
     paddingBottom: theme.spacing(8),
   },
+  header: {
+    display: "flex",
+    alignItems: "center",
+  },
   title: {
     padding: theme.spacing(2, 2, 0),
+  },
+  favButton: {
+    marginLeft: "auto",
+    color: "red",
   },
   expansionPanelHeading: {
     fontSize: `${theme.typography.pxToRem(14)}!important`,
@@ -55,6 +67,11 @@ export const Player = () => {
   const [player, setPlayer] = useState<PlayerRequest | null>(null);
   const [matches, setMatches] = useState<MatchesRequest | null>([]);
   const [error, setError] = useState("");
+
+  const [favoritePlayers, setFavoritePlayers] = useLocalStorage<String[]>(
+    "favoritePlayers",
+    []
+  );
 
   const loadMatches = async () => {
     setMatches(null);
@@ -89,13 +106,42 @@ export const Player = () => {
     player &&
     isBefore(sub(new Date(), { minutes: 60 }), parseISO(player.createdAt));
 
+  const onChangeFavorite = () => {
+    if (!favoritePlayers) return;
+    if (favoritePlayers.includes(id)) {
+      favoritePlayers.splice(favoritePlayers.indexOf(id));
+      setFavoritePlayers([...favoritePlayers]);
+    } else {
+      setFavoritePlayers([...favoritePlayers, id]);
+    }
+  };
+
   return (
     <Container maxWidth="md">
       <Grid container className={classes.root} spacing={2}>
-        <Grid item xs={12}>
+        <Grid item xs={12} className={classes.header}>
           <Typography className={classes.title} variant="h4" gutterBottom>
             {id}
           </Typography>
+          <Tooltip
+            title="Add to favorite"
+            placement="left"
+            enterDelay={500}
+            enterNextDelay={500}
+            disableFocusListener
+          >
+            <IconButton
+              className={classes.favButton}
+              size="medium"
+              onClick={() => onChangeFavorite()}
+            >
+              {favoritePlayers && favoritePlayers.includes(id) ? (
+                <FavoriteIcon fontSize="inherit" />
+              ) : (
+                <FavoriteBorderIcon fontSize="inherit" />
+              )}
+            </IconButton>
+          </Tooltip>
         </Grid>
 
         {isNewPlayer && (
@@ -117,6 +163,8 @@ export const Player = () => {
                 : "Updated: never"
             }
             placement="top-start"
+            enterDelay={500}
+            enterNextDelay={500}
             disableFocusListener
           >
             <Typography variant="subtitle1">Total stats</Typography>
@@ -140,6 +188,8 @@ export const Player = () => {
                 : "Updated: never"
             }
             placement="top-start"
+            enterDelay={500}
+            enterNextDelay={500}
             disableFocusListener
           >
             <Typography variant="subtitle1">Matches</Typography>
