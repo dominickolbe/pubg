d2599598dc84f664c794f96fda45285e99edc70a
Dominic Kolbe <kolbedominic@gmail.com>
2020-07-06 01:37:19 +0100

    Fix to get real client ip

--- packages/pubg-server/src/middlewares/audit.ts
+++ packages/pubg-server/src/middlewares/audit.ts
@@ -2,12 +2,19 @@ import Koa from "koa";
 import { AuditModel } from "../database/model/Audit";
 
 export const auditLog = async (ctx: Koa.Context, next: Koa.Next) => {
-  // TODO: this is just for testing
-  console.log(ctx.request.headers);
   try {
+    const getClientIP = () => {
+      if (ctx.request.headers["x-forwarded-for"]) {
+        return ctx.request.headers["x-forwarded-for"]
+          .split(",")
+          .reverse()[0]
+          .trim();
+      }
+      return ctx.request.ip || "";
+    };
     const audit = new AuditModel({
       request: {
-        ip: ctx.request.ip || "",
+        ip: getClientIP(),
         method: ctx.request.method,
         path: ctx.request.url,
       },
