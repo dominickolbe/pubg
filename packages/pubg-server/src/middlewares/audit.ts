0d7a890ce799d08277ddf356610f9dbb17eac42d
Dominic Kolbe <kolbedominic@gmail.com>
2020-07-06 01:25:50 +0100

    Add console log for testing

--- packages/pubg-server/src/middlewares/audit.ts
+++ packages/pubg-server/src/middlewares/audit.ts
@@ -2,6 +2,8 @@ import Koa from "koa";
 import { AuditModel } from "../database/model/Audit";
 
 export const auditLog = async (ctx: Koa.Context, next: Koa.Next) => {
+  // TODO: this is just for testing
+  console.log(ctx.request.headers);
   try {
     const audit = new AuditModel({
       request: {
