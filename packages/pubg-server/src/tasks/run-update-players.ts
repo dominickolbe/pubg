b42e863ede529c3e05dc97ad23374b9de1ec447c
Dominic Kolbe <kolbedominic@gmail.com>
2021-01-10 23:57:46 +0000

    Create importer task

--- packages/pubg-server/src/tasks/run-update-players.ts
+++ packages/pubg-server/src/tasks/run-update-players.ts
@@ -1,18 +1,44 @@
 require("dotenv-safe").config();
 
+import { HTTP_STATUS_TOO_MANY_REQUESTS } from "pubg-utils/src";
 import { Database } from "../database/mongo";
+import { PlayerModel } from "../database/mongo/model/player";
 import { redisDatabase } from "../database/redis";
+import { updatePlayerStatsAndMatches } from "../utils";
 
 const run = async () => {
-  const db = await Database.connect();
-  if (db.err) process.exit(1);
-
   console.log("[Info]: run-update-players");
+
+  const exit = async (exitCode: number) => {
+    await Database.disconnect();
+    process.exit(exitCode);
+  };
+
+  const db = await Database.connect();
+  if (db.err) exit(1);
+
+  const players = await PlayerModel.find({ autoUpdate: true });
+
+  if (!players || players.length === 0) {
+    console.log(`[Info]: no players found`);
+    return await exit(0);
+  }
+
+  console.log(`[Info]: ${players.length} players found`);
+
+  for (const player of players) {
+    console.log(`[Info]: start to import stats for "${player.name}" ...`);
+    const result = await updatePlayerStatsAndMatches(player);
+
+    if (result.err === HTTP_STATUS_TOO_MANY_REQUESTS) {
+      console.log(`[Error]: STOP importer. PUBG API LIMIT REACHED.`);
+      await exit(0);
+    }
+  }
 
   redisDatabase.flushdb();
 
-  await Database.disconnect();
-  process.exit(0);
+  await exit(0);
 };
 
 run();
