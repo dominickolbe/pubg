9057a02c6d926ff949a2183a00be23b2128cb302
Dominic Kolbe <kolbedominic@gmail.com>
2021-01-11 03:22:58 +0000

    CS logger

--- packages/pubg-server/src/utils/logger.ts
+++ packages/pubg-server/src/utils/logger.ts
@@ -2,7 +2,7 @@ import { createLogger, format, transports } from "winston";
 
 const { combine, timestamp, label, printf } = format;
 const customFormat = printf(({ level, message, label, timestamp }) => {
-  return `${timestamp} [${label}] ${level}: ${message}`;
+  return `${timestamp} [${label}] [${level}] ${message}`;
 });
 
 export const Logger = (params: { namespace: string }) => {
