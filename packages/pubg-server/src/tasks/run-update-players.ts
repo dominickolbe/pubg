958e682592adb49e67399441c92986e0a07ff0b5
Dominic Kolbe <kolbedominic@gmail.com>
2021-01-21 01:40:14 +0000

    CS

--- packages/pubg-server/src/tasks/run-update-players.ts
+++ packages/pubg-server/src/tasks/run-update-players.ts
@@ -1,19 +1,18 @@
+require("dotenv-safe").config();
+
 import { HTTP_STATUS_TOO_MANY_REQUESTS } from "pubg-utils/src";
 import { Database } from "../database/mongo";
 import { PlayerModel } from "../database/mongo/model/player";
 import { redisDatabase } from "../database/redis";
 import { updatePlayerStatsAndMatches } from "../utils";
-import { Logger } from "../utils/logger";
 
 const run = async () => {
-  const logger = Logger({ namespace: "run-update-players" });
-
-  logger.info("start job");
+  console.log("start job");
 
   const exit = async (exitCode: number) => {
     redisDatabase.end();
     await Database.disconnect();
-    logger.info("end job");
+    console.log("end job");
     process.exit(exitCode);
   };
 
@@ -22,20 +21,19 @@ const run = async () => {
 
   const players = await PlayerModel.find({ autoUpdate: true });
 
-  logger.info(`${players.length} players found`);
+  console.log(`${players.length} players found`);
 
   if (!players || players.length === 0) {
     return await exit(0);
   }
 
   for (const player of players) {
-    logger.info(`import stats for "${player.name}"`);
+    console.log(`import stats for "${player.name}"`);
 
     const result = await updatePlayerStatsAndMatches(player);
 
     if (result.err === HTTP_STATUS_TOO_MANY_REQUESTS) {
       console.log(`[Error]: STOP importer. PUBG API LIMIT REACHED.`);
-      logger.error("PUBG API LIMIT REACHED");
       await exit(0);
     }
   }
