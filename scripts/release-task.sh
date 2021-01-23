05d8653f1601b3dba2e14dca73aeed3509dbad95
Dominic Kolbe <kolbedominic@gmail.com>
2021-01-23 02:04:41 +0000

    CS

--- scripts/release-task.sh
+++ scripts/release-task.sh
@@ -15,8 +15,7 @@ sudo docker-compose --env-file packages/pubg-server/.env build
 echo "4. Restart docker instance"
 sudo docker-compose --env-file packages/pubg-server/.env up -d
 
-echo "5. chmod"
-sudo chmod +x scripts/release-task.sh
-sudo chmod +x scripts/run-update-players.sh
+echo "5. make scripts executable"
+sudo chmod +x -R scripts/
 
 echo "# # # Finished release task # # #"
